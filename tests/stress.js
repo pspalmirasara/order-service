a28e0b7b73d6e5fb4d194320f40ebb4210db35c6
pspalmirasaraaf <pspalmirasaraaf@gmail.com>
2024-08-05 01:07:04 -0300

    change nodeport

--- tests/stress.js
+++ tests/stress.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 
 export default function () {
-  http.get("http://localhost:30201/customers?cpf=12345678910");
+  http.get("http://localhost:30202/customers?cpf=12345678910");
 
 }
